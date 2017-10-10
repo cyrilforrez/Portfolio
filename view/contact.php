d0f66efedee89473d4e5496944f599d6fb04862a
cyrilforrez <cyilforrez@gmail.com>
2017-10-10 16:32:35 +0200

    add btn submit

--- view/contact.php
+++ view/contact.php
@@ -22,6 +22,9 @@
         </div>
       </div>
       </div>
+      <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+       <i class="material-icons right">send</i>
+     </button>
     </div>
   </form>
 </div>
